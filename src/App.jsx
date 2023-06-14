d81b87d930cfdd32378687772a21562d8bc64699
marveltaylor <marveltaylor85@gmail.com>
2023-06-13 18:37:41 -0700

    updated useState hook and deconstructed selectedColor and setSelectedColor inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react"; //import useState hook from React
 
 // Write your Color component here
-const [selectedColor, setSelectedColor] = useState(""); //invoke useState hook and deconstruct selectedColor and setSelectedColor from the returned array
 //const Color = (props) => { //using dot notation use prop as className to access color from props object
   // return <div className = {props.color}></div> 
 const Color = ({color, setSelectedColor}) => { //using object deconstruction to access the color from props object
   return (
-  <div className = {color} onClick = {() => setSelectedColor(color)}></div>
+  <div className = {color} onClick = {() => setSelectedColor(color)}></div> //added onClick property using object deconstruction
   )
 }
 
 const App = () => {
+const [selectedColor, setSelectedColor] = useState(""); //invoke useState hook and deconstruct selectedColor and setSelectedColor from the returned array
   return (
     <div id = "container">
       <div id = "navbar">
