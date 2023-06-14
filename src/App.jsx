750f67354133fc5320ebd6ebcfac0134bb091e0a
marveltaylor <marveltaylor85@gmail.com>
2023-06-13 18:10:45 -0700

    updated from red to selectedColor in the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <div id = "container">
       <div id = "navbar">
         <div>Currently selected: </div>
-        <div className = "red">red</div>
+        <div className = {selectedColor}>{selectedColor}</div>
       </div>
       <div id = "colors-list">
         <Color color = "green"/>
