35ae44bf3df8d943b36c7ab37dd445014e1c378e
marveltaylor <marveltaylor85@gmail.com>
2023-06-13 17:48:24 -0700

    updated Color component to use prop as className

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 // Write your Color component here
 const Color = (props) => {
-  return <div className="red"></div>
+  return <div className = {props.color}></div>
 }
 
 const App = () => {
   return (
-    <div id="container">
-      <div id="navbar">
+    <div id = "container">
+      <div id = "navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className = "red">red</div>
       </div>
-      <div id="colors-list">
-        <Color color="green"/>
-        <Color color="blue"/>
-        <Color color="violet"/>
+      <div id = "colors-list">
+        <Color color = "green"/>
+        <Color color = "blue"/>
+        <Color color = "violet"/>
       </div>
     </div>
   );
